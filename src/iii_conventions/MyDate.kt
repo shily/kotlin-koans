04e7e64bb84cc9b03da748fa8e53d887ca086ea7
Liu Shuai <liushuai05@baidu.com>
2017-06-27 15:28:03 +0800

    change: answers for iii_conventions

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,13 +1,55 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int {
+        return if (year != other.year) {
+            year - other.year
+        } else if (month != other.month) {
+            month - other.month
+        } else {
+            dayOfMonth - other.dayOfMonth
+        }
+    }
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+    operator fun plus(interval: TimeInterval): MyDate {
+        return addTimeIntervals(interval, 1)
+    }
+
+    operator fun plus(days: Int): MyDate {
+        return addTimeIntervals(TimeInterval.DAY, days)
+    }
+}
+
+operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other)
 
 enum class TimeInterval {
     DAY,
     WEEK,
-    YEAR
+    YEAR;
+
+    operator fun times(i: Int): Int {
+        return when (this) {
+            DAY -> i
+            WEEK -> i * 7
+            YEAR -> i * 365
+        }
+    }
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+class DateRange(override val start: MyDate, override val endInclusive: MyDate) : ClosedRange<MyDate>, Iterable<MyDate> {
+    override fun iterator(): Iterator<MyDate> {
+        var current: MyDate = start
+        val dateRange = start..endInclusive
+        return object : Iterator<MyDate> {
+            override fun hasNext(): Boolean {
+                return current in dateRange
+            }
+
+            override fun next(): MyDate {
+                val next = current
+                current = current.nextDay()
+                return next
+            }
+        }
+    }
+}
--- src/iii_conventions/_29_OperatorsOverloading.kt
+++ src/iii_conventions/_29_OperatorsOverloading.kt
@@ -4,7 +4,7 @@ import util.TODO
 import iii_conventions.TimeInterval.*
 
 fun todoTask29(): Nothing = TODO(
-    """
+        """
         Task 29.
         Implement a kind of date arithmetic. Support adding years, weeks and days to a date.
         Use classes MyDate and TimeInterval.
@@ -15,17 +15,17 @@ fun todoTask29(): Nothing = TODO(
         (2). Support adding several time intervals to a date. Add an extra class.
         If you have any problems, see the iii_conventions/_29_Tips.kt file.
     """,
-    references = { date: MyDate, timeInterval: TimeInterval ->
-        date.addTimeIntervals(timeInterval, 1)
-    })
+        references = { date: MyDate, timeInterval: TimeInterval ->
+            date.addTimeIntervals(timeInterval, 1)
+        })
 
 fun task29_1(today: MyDate): MyDate {
-    todoTask29()
-//    return today + YEAR + WEEK
+//    todoTask29()
+    return today + YEAR + WEEK
 }
 
 fun task29_2(today: MyDate): MyDate {
-    todoTask29()
-//    return today + YEAR * 2 + WEEK * 3 + DAY * 5
+//    todoTask29()
+    return today + YEAR * 2 + WEEK * 3 + DAY * 5
 }
 
